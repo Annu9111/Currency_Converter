16e3f07760a03530c7abc27ac43660292f03626d
Annu9111 <aksoni911190@gmail.com>
2026-03-23 13:25:27 +0530

    js

--- script.js
+++ script.js
@@ -7,6 +7,19 @@ for (let select of dropdowns){
         let newOption=document.createElement("option");
         newOption.innerText=currCode;
         newOption.value=currCode;
+        if (select.name==="from" && currCode === "USD"){
+            newOption.selected="selected";
+        }else if (select.name === "to" && currCode ==="INR"){
+            newOption.selected="selected";
+        }
         select.append(newOption);
     }
+    select.addEventListener("change",(evt)=>{
+        updateFlag(evt.target);
+    })
+}
+
+const updateFlag=(element)=>{
+    let currCode=element.value;
+    let countryCode=countryList[currCode];
 }
